c6f453106364a02dda159b191a71797c964c5300
zbryikt <tkirby@gmail.com>
2023-10-06 01:38:01 +0800

    update

---
+++ web/.view/index.js
@@ -0,0 +1,184 @@
+ (function() { function pug_attr(t,e,n,r){if(!1===e||null==e||!e&&("class"===t||"style"===t))return"";if(!0===e)return" "+(r?t:t+'="'+t+'"');var f=typeof e;return"object"!==f&&"function"!==f||"function"!=typeof e.toJSON||(e=e.toJSON()),"string"==typeof e||(e=JSON.stringify(e),n||-1===e.indexOf('"'))?(n&&(e=pug_escape(e))," "+t+'="'+e+'"'):" "+t+"='"+e.replace(/'/g,"&#39;")+"'"}
+function pug_escape(e){var a=""+e,t=pug_match_html.exec(a);if(!t)return e;var r,c,n,s="";for(r=t.index,c=0;r<a.length;r++){switch(a.charCodeAt(r)){case 34:n="&quot;";break;case 38:n="&amp;";break;case 60:n="&lt;";break;case 62:n="&gt;";break;default:continue}c!==r&&(s+=a.substring(c,r)),c=r+1,s+=n}return c!==r?s+a.substring(c,r):s}
+var pug_match_html=/["&<>]/;function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;;
+    var locals_for_with = (locals || {});
+    
+    (function (Array, c, defer, hashfile, libLoader, md5, url, version) {
+      pug_html = pug_html + "\u003C!DOCTYPE html\u003E";
+if(!libLoader) {
+  libLoader = {
+    js: {url: {}},
+    css: {url: {}},
+    root: function(r) { libLoader._r = r; },
+    _r: "/assets/lib",
+    _v: "",
+    version: function(v) { libLoader._v = (v ? "?v=" + v : ""); }
+  }
+  if(version) { libLoader.version(version); }
+}
+
+pug_mixins["script"] = pug_interp = function(os,cfg){
+var block = (this && this.block), attributes = (this && this.attributes) || {};
+var str = '', urls = [];
+if(!Array.isArray(os)) { os = [os]; }
+// iterate os
+;(function(){
+  var $$obj = os;
+  if ('number' == typeof $$obj.length) {
+      for (var pug_index0 = 0, $$l = $$obj.length; pug_index0 < $$l; pug_index0++) {
+        var o = $$obj[pug_index0];
+c = o;
+if(typeof(o) == "string") { url = o; c = cfg || {};}
+else if(o.url) { url = o.url; }
+else { url = libLoader._r + "/" + o.name + "/" + (o.version || 'main') + "/" + (o.path || "index.min.js"); }
+if (!libLoader.js.url[url]) {
+libLoader.js.url[url] = true;
+defer = (typeof(c.defer) == "undefined" ? true : !!c.defer);
+if (/^https?:\/\/./.exec(url)) {
+pug_html = pug_html + "\u003Cscript" + (" type=\"text\u002Fjavascript\""+pug_attr("src", url, true, true)+pug_attr("defer", defer, true, true)+pug_attr("async", !!c.async, true, true)) + "\u003E\u003C\u002Fscript\u003E";
+}
+else
+if (cfg && cfg.pack) {
+str = str + ';' + url;
+urls.push(url);
+}
+else {
+pug_html = pug_html + "\u003Cscript" + (" type=\"text\u002Fjavascript\""+pug_attr("src", url + libLoader._v, true, true)+pug_attr("defer", defer, true, true)+pug_attr("async", !!c.async, true, true)) + "\u003E\u003C\u002Fscript\u003E";
+}
+}
+      }
+  } else {
+    var $$l = 0;
+    for (var pug_index0 in $$obj) {
+      $$l++;
+      var o = $$obj[pug_index0];
+c = o;
+if(typeof(o) == "string") { url = o; c = cfg || {};}
+else if(o.url) { url = o.url; }
+else { url = libLoader._r + "/" + o.name + "/" + (o.version || 'main') + "/" + (o.path || "index.min.js"); }
+if (!libLoader.js.url[url]) {
+libLoader.js.url[url] = true;
+defer = (typeof(c.defer) == "undefined" ? true : !!c.defer);
+if (/^https?:\/\/./.exec(url)) {
+pug_html = pug_html + "\u003Cscript" + (" type=\"text\u002Fjavascript\""+pug_attr("src", url, true, true)+pug_attr("defer", defer, true, true)+pug_attr("async", !!c.async, true, true)) + "\u003E\u003C\u002Fscript\u003E";
+}
+else
+if (cfg && cfg.pack) {
+str = str + ';' + url;
+urls.push(url);
+}
+else {
+pug_html = pug_html + "\u003Cscript" + (" type=\"text\u002Fjavascript\""+pug_attr("src", url + libLoader._v, true, true)+pug_attr("defer", defer, true, true)+pug_attr("async", !!c.async, true, true)) + "\u003E\u003C\u002Fscript\u003E";
+}
+}
+    }
+  }
+}).call(this);
+
+if (cfg && cfg.pack) {
+var name = md5(str);
+//var filename = "/js/pack/" + name + "." + (typeof(cfg.min) == "undefined" || cfg.min ? "min" : "") + ".js";
+var fn = "/assets/bundle/" + name + "." + (typeof(cfg.min) == "undefined" || cfg.min ? "min" : "") + ".js";
+hashfile({type: "js", name: name, files: urls, src: locals.filename});
+pug_html = pug_html + "\u003Cscript" + (" type=\"text\u002Fjavascript\""+pug_attr("src", fn + libLoader._v, true, true)) + "\u003E\u003C\u002Fscript\u003E";
+}
+};
+pug_mixins["css"] = pug_interp = function(os,cfg){
+var block = (this && this.block), attributes = (this && this.attributes) || {};
+var str = '', urls = [];
+if(!Array.isArray(os)) { os = [os]; }
+// iterate os
+;(function(){
+  var $$obj = os;
+  if ('number' == typeof $$obj.length) {
+      for (var pug_index1 = 0, $$l = $$obj.length; pug_index1 < $$l; pug_index1++) {
+        var o = $$obj[pug_index1];
+c = o;
+if(typeof(o) == "string") { url = o; c = cfg || {};}
+else if(o.url) { url = o.url; }
+else { url = libLoader._r + "/" + o.name + "/" + (o.version || 'main') + "/" + (o.path || "index.min.css"); }
+if (!libLoader.css.url[url]) {
+libLoader.css.url[url] = true;
+if (/^https?:\/\/./.exec(url)) {
+pug_html = pug_html + "\u003Clink" + (" rel=\"stylesheet\" type=\"text\u002Fcss\""+pug_attr("href", url, true, true)) + "\u003E";
+}
+else
+if (cfg && cfg.pack) {
+str = str + ';' + url;
+urls.push(url);
+}
+else {
+pug_html = pug_html + "\u003Clink" + (" rel=\"stylesheet\" type=\"text\u002Fcss\""+pug_attr("href", url + libLoader._v, true, true)) + "\u003E";
+}
+}
+      }
+  } else {
+    var $$l = 0;
+    for (var pug_index1 in $$obj) {
+      $$l++;
+      var o = $$obj[pug_index1];
+c = o;
+if(typeof(o) == "string") { url = o; c = cfg || {};}
+else if(o.url) { url = o.url; }
+else { url = libLoader._r + "/" + o.name + "/" + (o.version || 'main') + "/" + (o.path || "index.min.css"); }
+if (!libLoader.css.url[url]) {
+libLoader.css.url[url] = true;
+if (/^https?:\/\/./.exec(url)) {
+pug_html = pug_html + "\u003Clink" + (" rel=\"stylesheet\" type=\"text\u002Fcss\""+pug_attr("href", url, true, true)) + "\u003E";
+}
+else
+if (cfg && cfg.pack) {
+str = str + ';' + url;
+urls.push(url);
+}
+else {
+pug_html = pug_html + "\u003Clink" + (" rel=\"stylesheet\" type=\"text\u002Fcss\""+pug_attr("href", url + libLoader._v, true, true)) + "\u003E";
+}
+}
+    }
+  }
+}).call(this);
+
+if (cfg && cfg.pack) {
+var name = md5(str);
+//var filename = "/css/pack/" + name + "." + (typeof(cfg.min) == "undefined" || cfg.min ? "min" : "") + ".css";
+var fn = "/assets/bundle/" + name + "." + (typeof(cfg.min) == "undefined" || cfg.min ? "min" : "") + ".css";
+hashfile({type: "css", name: name, files: urls, src: locals.filename});
+pug_html = pug_html + "\u003Clink" + (" rel=\"stylesheet\" type=\"text\u002Fcss\""+pug_attr("href", fn + libLoader._v, true, true)) + "\u003E";
+}
+};
+pug_html = pug_html + "\u003Chtml\u003E\u003Chead\u003E\u003Cmeta charset=\"utf-8\"\u003E\u003Clink rel=\"preconnect\" href=\"https:\u002F\u002Ffonts.googleapis.com\"\u003E\u003Clink" + (" rel=\"preconnect\" href=\"https:\u002F\u002Ffonts.gstatic.com\""+pug_attr("crossorigin", true, true, true)) + "\u003E\u003Clink href=\"https:\u002F\u002Ffonts.googleapis.com\u002Fcss2?family=Noto+Sans+TC:wght@300&amp;family=Noto+Serif+TC:wght@900&amp;display=swap\" rel=\"stylesheet\"\u003E";
+pug_mixins["css"]([
+      {name: "bootstrap", path: "dist/css/bootstrap.min.css"},
+      {name: "@loadingio/bootstrap.ext"},
+      {name: "ldiconfont"},
+      {name: "ldlazy"},
+    ]);
+pug_html = pug_html + "\u003Cstyle type=\"text\u002Fcss\"\u003Ebody,html{width:100%;height:100%;margin:0;padding:0;font-family:'Noto Sans TC',sans-serif}body{background:url(\"\u002Fassets\u002Fimg\u002Fnami.svg\") center center no-repeat fixed;background-size:cover}#hero-image{display:block;margin:2em auto;max-width:90%;width:768px}a{color:#011c2f;font-weight:900;font-family:'Noto Serif TC',serif}\u003C\u002Fstyle\u003E\u003C\u002Fhead\u003E\u003Cbody\u003E\u003Cimg id=\"hero-image\" src=\"\u002Fassets\u002Fimg\u002Findex.svg\"\u003E\u003Cdiv class=\"d-flex g-4 text-lg mx-auto w-1024 rwd justify-content-center my-5\"\u003E\u003Ca href=\"https:\u002F\u002Ffuturecity.cw.com.tw\u002F\"\u003E\u003Ci class=\"i-globe\"\u003E\u003C\u002Fi\u003E 未來城市\u003C\u002Fa\u003E\u003Ca href=\"https:\u002F\u002Fwww.facebook.com\u002FEditorLikeSiren\"\u003E\u003Ci class=\"i-fb\"\u003E\u003C\u002Fi\u003E 像我這樣的一個主編\u003C\u002Fa\u003E\u003C\u002Fdiv\u003E";
+pug_mixins["script"]([
+      {name: "proxise"},
+      {name: "@loadingio/debounce.js"},
+      {name: "@loadingio/ldquery"},
+      {name: "ldview"},
+      {name: "ldlazy"},
+      {name: "ldcover"}
+    ]);
+pug_html = pug_html + "\u003C\u002Fbody\u003E\u003C\u002Fhtml\u003E";
+    }.call(this, "Array" in locals_for_with ?
+        locals_for_with.Array :
+        typeof Array !== 'undefined' ? Array : undefined, "c" in locals_for_with ?
+        locals_for_with.c :
+        typeof c !== 'undefined' ? c : undefined, "defer" in locals_for_with ?
+        locals_for_with.defer :
+        typeof defer !== 'undefined' ? defer : undefined, "hashfile" in locals_for_with ?
+        locals_for_with.hashfile :
+        typeof hashfile !== 'undefined' ? hashfile : undefined, "libLoader" in locals_for_with ?
+        locals_for_with.libLoader :
+        typeof libLoader !== 'undefined' ? libLoader : undefined, "md5" in locals_for_with ?
+        locals_for_with.md5 :
+        typeof md5 !== 'undefined' ? md5 : undefined, "url" in locals_for_with ?
+        locals_for_with.url :
+        typeof url !== 'undefined' ? url : undefined, "version" in locals_for_with ?
+        locals_for_with.version :
+        typeof version !== 'undefined' ? version : undefined));
+    ;;return pug_html;}; module.exports = template; })() 
---
+++ web/static/assets/lib/ldlazy/2.0.0/CHANGELOG.md
@@ -0,0 +1,30 @@
+# Change Log
+
+## v2.0.0
+
+ - check for existence instead of value for populating ldlazy in window
+ - upgrade modules
+ - release with compact directory structure
+ - add `style` in `package.json`
+ - add `main` and `browser` field in `package.json`.
+ - patch test code to make it work with upgraded modules
+
+
+## v1.0.2
+
+ - minimize js further with -c -m options
+
+
+## v1.0.1
+
+ - use a shorter gif placeholder in ldlz mixin
+
+
+## v1.0.0
+
+ - rename `ldlz` in dist to `index`
+ - rename `ldLazy` to `ldlazy`
+ - rename mixin `lzimg` to `ldlz` in Pug
+ - disable init by default, but make it as a function
+ - provide 1px gif as `ldlazy.px`
+ - better documentation
